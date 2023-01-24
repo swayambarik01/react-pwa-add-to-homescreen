238181c28003606b08cc29a6f8c0eb6406935e35
swayambarik01 <100610987+swayambarik01@users.noreply.github.com>
2023-01-23 16:14:39 -0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-export const TRANSLATE_SAFARI_TAP_SHARE = 'Tap "Share"';
+export const TRANSLATE_SAFARI_TAP_SHARE = 'To Install Zome App: Tap "Share"';
 export const TRANSLATE_SAFARI_ADD_HOME_SCREEN = 'select "Add to Home Screen"';
-export const TRANSLATE_CHROME_ADD_HOME_SCREEN = 'Click "Add to Home Screen" in your browser menu';
+export const TRANSLATE_CHROME_ADD_HOME_SCREEN = 'To Install Zome App: Click "Add to Home Screen" in your browser menu';
 export const TRANSLATE_CHROME_INSTALL = 'The app icon will be displayed on your screen';
 export const TRANSLATE_INSTALL_BUTTON = 'install';
 
